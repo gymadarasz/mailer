4b009e97343f0df1a143c9ce400fee870b29a3ca
Gyula Madarasz <gyula.madarasz@gmail.com>
2021-05-18 16:29:20 +0100

    form labels

--- tpls/mailer/mailer.html.php
+++ tpls/mailer/mailer.html.php
@@ -1,18 +1,23 @@
 <form method="POST" action="{$__base}mailer">
     <input type="hidden" name="csrf" value="{$__csrf}" />
     
+    <label>To email address</label>
     <input class="form-control"  type="email" name="to_email" value="{@$to_email}" placeholder="To email address" required />
     <br />
 
+    <label>From email address</label>
     <input class="form-control"  type="email" name="from_email" value="{@$from_email}" placeholder="From email address" required />
     <br />
     
+    <label>From displayed name</label>
     <input class="form-control"  type="text" name="from_name" value="{@$from_name}" placeholder="From displayed name" required />
     <br />
     
+    <label>Subject</label>
     <input class="form-control"  type="text" name="subject" value="{@$subject}" placeholder="Subject" required />
     <br />
     
+    <label>Body</label>
     <textarea class="form-control"  name="body" placeholder="Body" rows="12" required>{@$body}</textarea>
     <br />
     
